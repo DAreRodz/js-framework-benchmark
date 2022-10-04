1462a7c43799e100320a3056fe0b21bd0e6d04a2
David Arenas <david.arenas@automattic.com>
2022-10-04 15:05:52 +0200

    Move context data inside `rows`

--- frameworks/keyed/wp-directives/src/main.js
+++ frameworks/keyed/wp-directives/src/main.js
@@ -1,14 +1,13 @@
-import "preact/debug";
 import wpx from "./wpx";
 import buildData from "./build-data";
 
 wpx({
   state: {
     data({ context }) {
-      return context.data;
+      return context.rows.data;
     },
     isSelected({ context }) {
-      return context.selected === context.item.id;
+      return context.rows.selected === context.item.id;
     },
     itemId({ context }) {
       return context.item.id;
@@ -19,34 +18,34 @@ wpx({
   },
   actions: {
     add({ context }) {
-      context.data = context.data.concat(buildData(1000));
+      context.rows.data = context.rows.data.concat(buildData(1000));
     },
     clear({ context }) {
-      context.data = [];
-      context.selected = undefined;
+      context.rows.data = [];
+      context.rows.selected = undefined;
     },
     update({ context }) {
-      for (let i = 0; i < context.data.length; i += 10) {
-        context.data[i].label += " !!!";
+      for (let i = 0; i < context.rows.data.length; i += 10) {
+        context.rows.data[i].label += " !!!";
       }
     },
     remove({ context }) {
-      const idx = context.data.findIndex((d) => d.id === context.item.id);
-      context.data.splice(idx, 1);
+      const idx = context.rows.data.findIndex((d) => d.id === context.item.id);
+      context.rows.data.splice(idx, 1);
     },
     run({ context }) {
-      context.data = buildData(1000);
-      context.selected = undefined;
+      context.rows.data = buildData(1000);
+      context.rows.selected = undefined;
     },
     runLots({ context }) {
-      context.data = buildData(10000);
-      context.selected = undefined;
+      context.rows.data = buildData(10000);
+      context.rows.selected = undefined;
     },
     select({ context }) {
-      context.selected = context.item.id;
+      context.rows.selected = context.item.id;
     },
     swapRows({ context }) {
-      const d = context.data;
+      const d = context.rows.data;
       if (d.length > 998) {
         const tmp = d[998];
         d[998] = d[1];
